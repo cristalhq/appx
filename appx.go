b958bf59ea3b0599bba730a00b22fd0b099563a5
Oleg Kovalov <oleg@hey.com>
2022-09-08 16:58:22 +0200

    Add Uptime

--- appx.go
+++ appx.go
@@ -4,10 +4,18 @@ import (
 	"context"
 	"os/signal"
 	"syscall"
+	"time"
 )
+
+var startTime = time.Now()
 
 // Context of the application listening SIGINT and SIGTERM signals.
 func Context() context.Context {
 	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	return ctx
+}
+
+// Uptime of the application.
+func Uptime() time.Duration {
+	return time.Since(startTime)
 }
